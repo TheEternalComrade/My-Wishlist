26ba2aa7380cfa3027a4cb8a5a7005d52eefe6f2
TheEternalComrade <taconora52@gmail.com>
2024-12-31 13:35:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ const observer = new IntersectionObserver((entries) => {
         }
     });
 });
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add("show")
+        }
+    });
+});
 const hiddenElements = document.querySelectorAll(".hidden")
+const wishes = document.querySelectorAll(".wish")
 
 hiddenElements.forEach((el) => observer.observe(el))
+
+wishes.forEach((el) => observer2.observe(el))
+
